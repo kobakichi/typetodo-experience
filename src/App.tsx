0645b701d03b176e3e4d3466ce4cde2f69628f9f
kobakichi <fenderstrat57vin@gmail.com>
2022-05-09 05:52:48 +0900

    createを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  // inputformからの入力を受け取るstate
+  const [inputValue, setInputValue] = useState("");
+
+  // useStateにはTodoで指定した型しか適用させない　　この配列には指定した型しか入らない
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  // 型の指定
+  type Todo = {
+    inputValue: string;
+    id: number;
+    checked: boolean;
+  };
+
+  // inputformで受け取った値を取得する関数
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // console.log(e.target.value);
+    setInputValue(e.target.value);
+  };
+
+  // 送信ボタンを押した時の処理の流れ
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // 新しいTodoを作成
+    const newTodo: Todo = {
+      inputValue: inputValue,
+      id: todos.length,
+      checked: false,
+    };
+    // todosを展開して、newTodoへ値を格納して、最後にinputformの中を空にする
+    setTodos([newTodo, ...todos]);
+    setInputValue("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h2>TypeScript TodoApp</h2>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input type="text" onChange={(e) => handleChange(e)} />
+        <input type="submit" value="作成" />
+      </form>
     </div>
   );
 }
